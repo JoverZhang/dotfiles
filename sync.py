00276ce25c15aaace91d82e54e830ee0f193bade
JoverZhang <joverzh@gmail.com>
2023-03-30 06:15:51 +0800

    add neovim

--- sync.py
+++ sync.py
@@ -5,7 +5,9 @@ import os
 
 map = {
   '$HOME/DotFiles/config/i3': '$HOME/.config/i3',
+  '$HOME/DotFiles/config/neovim': '$HOME/.config/nvim',
   '$HOME/DotFiles/config/proxychains': '$HOME/.config/proxychains',
+  '$HOME/DotFiles/config/zsh': '$HOME/.config/zsh',
 }
 
 
@@ -13,9 +15,14 @@ def expandvars(var):
     return os.path.expandvars(var)
 
 def link(fm, to):
-    if os.path.samefile(expandvars(fm), expandvars(to)):
+    fm = os.path.expandvars(fm)
+    to = os.path.expandvars(to)
+
+    if os.path.exists(to) and os.path.samefile(fm, to):
+        print(f'SKIP: {to}')
         return
     os.system(f'ln -s {fm} {to}')
+    print(f'SYNC: {to}')
 
 
 for key in map:

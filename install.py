815a6e825c5726f3860bfed237522c2218930248
JoverZhang <joverzh@gmail.com>
2023-03-30 13:12:14 +0800

    Format Scripts

--- install.py
+++ install.py
@@ -2,19 +2,19 @@
 
 import os
 
-
 map = {
-  '$HOME/DotFiles/config/h_command': '$HOME/.config/h_command',
-  '$HOME/DotFiles/config/i3': '$HOME/.config/i3',
-  '$HOME/DotFiles/config/neovim': '$HOME/.config/nvim',
-  '$HOME/DotFiles/config/picom.conf': '$HOME/.config/picom.conf',
-  '$HOME/DotFiles/config/proxychains': '$HOME/.config/proxychains',
-  '$HOME/DotFiles/config/zsh': '$HOME/.config/zsh',
+    '$HOME/DotFiles/config/h_command': '$HOME/.config/h_command',
+    '$HOME/DotFiles/config/i3': '$HOME/.config/i3',
+    '$HOME/DotFiles/config/neovim': '$HOME/.config/nvim',
+    '$HOME/DotFiles/config/picom.conf': '$HOME/.config/picom.conf',
+    '$HOME/DotFiles/config/proxychains': '$HOME/.config/proxychains',
+    '$HOME/DotFiles/config/zsh': '$HOME/.config/zsh',
 }
 
 
 def expandvars(var):
     return os.path.expandvars(var)
+
 
 def link(fm, to):
     fm = os.path.expandvars(fm)
--- sync.py
+++ sync.py
@@ -1,15 +1,17 @@
-#!/bin/python
+#!/bin/python3
 
 import os
+
 
 def sync(module):
     print(f'SYNC: {module}')
     os.system(f'git submodule update --recursive --remote --depth 1 {module}')
 
+
 modules = [
     'config/zsh/oh-my-zsh',
     'config/zsh/zsh-autosuggestions',
-     'config/zsh/zsh-syntax-highlighting',
+    'config/zsh/zsh-syntax-highlighting',
     'fzf',
 ]
 

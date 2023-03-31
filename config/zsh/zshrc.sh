5df2bee27e7324a4ae198f64eaf4bae295df3d80
JoverZhang <joverzh@gmail.com>
2023-03-31 10:24:40 +0800

    Fix C-F

--- config/zsh/zshrc.sh
+++ config/zsh/zshrc.sh
@@ -166,7 +166,7 @@ fshow() {
 fzf-ls-cd-widget() {
 	local cmd="ls -al --color=yes | sed 1,2d"
 	setopt localoptions pipefail no_aliases 2>/dev/null
-	local dir="$(eval "$cmd" | FZF_DEFAULT_OPTS="--ansi --nth=8 --height ${FZF_TMUX_HEIGHT:-50%} --reverse --preview='if [ -f {8} ]; then bat -pn --color=always {8}; else ls -alH --color=yes {8}; fi' | awk '{ print $8 }' --bind=ctrl-z:ignore ${FZF_DEFAULT_OPTS-} ${FZF_ALT_C_OPTS-}" $(__fzfcmd) +m | awk '{ print $8 }')"
+	local dir="$(eval "$cmd" | FZF_DEFAULT_OPTS="--ansi --nth=9 --height ${FZF_TMUX_HEIGHT:-50%} --reverse --preview='if [ -f {9} ]; then bat -pn --color=always {9}; else ls -alH --color=yes {9}; fi' | awk '{ print \$9 }' --bind=ctrl-z:ignore ${FZF_DEFAULT_OPTS-} ${FZF_ALT_C_OPTS-}" $(__fzfcmd) +m | awk '{ print $9 }')"
 
 	# skip
 	if [[ -z "$dir" ]]; then

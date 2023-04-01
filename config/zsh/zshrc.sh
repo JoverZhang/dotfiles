54fc724b1a89c08bfea8d9c6096ea5903d1c3efe
JoverZhang <joverzh@gmail.com>
2023-04-01 16:45:28 +0800

    Update zshrc.sh

--- config/zsh/zshrc.sh
+++ config/zsh/zshrc.sh
@@ -11,7 +11,9 @@ export ZSH="$ZSH_ROOT/oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="jonathan"
+if [ -z "$ZSH_THEME" ]; then
+	ZSH_THEME="jonathan"
+fi
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
